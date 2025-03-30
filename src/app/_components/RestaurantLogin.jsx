0e772a41b6e48cbc1a0bb5e18349b2b7067f80a4
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-30 09:49:58 +0530

    Enhance RestaurantLogin component with form elements and styling

--- src/app/_components/RestaurantLogin.jsx
+++ src/app/_components/RestaurantLogin.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const RestaurantLogin = () => {
   return (
-    <div>RestaurantLogin</div>
+    <div className="p-8 flex flex-col justify-center items-center gap-4">
+      <h3 className='text-4xl'>Login</h3>
+      <form className='flex flex-col gap-2 max-w-1/2'>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button className='bg-blue-500 p-2 rounded-md w-full' type="submit">Login</button>
+      </form>
+    </div>
   )
 }
 
